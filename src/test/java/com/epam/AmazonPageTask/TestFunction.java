33e75a1c83f131a059932f6b663d8afb8094e6cf
Pavan Kotha <Pavan_Kotha@epam.com>
2018-10-01 15:31:09 +0530

    changed

--- src/test/java/com/epam/AmazonPageTask/TestFunction.java
+++ src/test/java/com/epam/AmazonPageTask/TestFunction.java
@@ -1,11 +1,9 @@
 package com.epam.AmazonPageTask;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
